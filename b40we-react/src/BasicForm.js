7c4fcb6ef0de7c128096960951540154ca2586ca
SangeethaShanmugm <sanjuriya22@gmail.com>
2023-01-29 12:47:09 +0530

    added formik and yup

--- b40we-react/src/BasicForm.js
+++ b40we-react/src/BasicForm.js
@@ -1,8 +1,19 @@
 import { useFormik } from "formik";
+import * as yup from "yup";
+
+const formValidationSchema = yup.object({
+  email: yup.string().min(5, "Need a longer email"),
+  password: yup
+    .string()
+    .min(8, "Need a longer password🥳")
+    .max(12, "Too much password😉")
+    .required("Why not fill this password"),
+});
 
 export function BasicForm() {
   const formik = useFormik({
     initialValues: { email: "", password: "" },
+    validationSchema: formValidationSchema,
     onSubmit: (values) => {
       console.log("onSubmit", values);
     },
@@ -17,6 +28,9 @@ export function BasicForm() {
         type="email"
         placeholder="Enter email"
       />
+      <br />
+      {formik.errors.email}
+      <br />
       <input
         id="password"
         name="password"
@@ -25,6 +39,9 @@ export function BasicForm() {
         type="password"
         placeholder="Enter password"
       />
+      <br />
+      {formik.errors.password}
+      <br />
       <button type="submit">Submit</button>
     </form>
   );
